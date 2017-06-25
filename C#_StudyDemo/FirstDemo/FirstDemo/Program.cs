14fd61c25397b5843a6f4fa608f13ab51d18ee20
SYCHS <429301805@qq.com>
2017-06-25 16:25:26 +0800

    学习C#的第一个程序，向控制台输出内容

---
+++ C#_StudyDemo/FirstDemo/FirstDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File-->New-->Project
+            //快捷键F5可以运行程序
+            //所有C#代码文件都使用.cs文件扩展名
+            //output text to the screen
+            Console.WriteLine("This is my first Demo to study C# ");
+            Console.ReadKey();//告诉代码在结束前等待按键，解决执行完毕后就立即终止的现象
+        }
+    }
+}
