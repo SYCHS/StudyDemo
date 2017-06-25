bb9bd96c4c28b513e746f32007ba95827ce5d8ab
SYCHS <429301805@qq.com>
2017-06-25 16:59:11 +0800

    WPF Application

---
+++ C#_StudyDemo/WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //点击按钮后弹出消息框
+            //WPF应用程序能够保持应用程序的设计（如按钮上的文本）与功能（如单击按钮后发生的操作）的分离
+            MessageBox.Show("This is my first desktop app to study C#");
+        }
+    }
+}
