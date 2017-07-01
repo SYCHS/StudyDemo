d9ad2fbed794abd2b58e0bc63f5f3cb2cdcbbbd1
SYCHS <429301805@qq.com>
2017-07-01 16:23:01 +0800

    do\for\while循坏

---
+++ C#_StudyDemo/Loop_8/Loop_8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_8
+{
+    class Program
+    {
+        /*
+         * 4.3、循坏：对同一代码操作多次。
+         *      循坏主要有三种：
+         *             1.do循坏
+         *             2.while循坏
+         *             3.for循坏
+         *             
+         * 4.3.1、do循坏
+         *      do循坏的结构：
+         *      do
+         *      {
+         *         <code to be looped>
+         *      }while(<Test>);
+         *      
+         * 4.3.2、while循坏
+         *      while(<Test>){
+         *        <code toi be looped>;
+         *      }
+         *      
+         * 4.3.3、for循坏
+         *     for(<initialization>;<codition>;<operation>){
+         *         <code to be looped>;
+         *     }
+         * 
+         * 4.3.4、循坏的中断
+         *      1.break——立即终止循坏
+         *      2.continue——立即终止当前的循坏（继续执行下一次循坏）。
+         *      3.return——跳出循坏及包含该循环的函数
+         * 
+         * 4.3.5、无限循坏
+         *       while(true){
+         *          //code in loop
+         *       }
+         */ 
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
