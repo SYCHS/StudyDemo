daeb91acf39e5fa7b4f8079d68714954f4d02e2d
SYCHS <429301805@qq.com>
2017-07-09 12:40:49 +0800

    22.嵌套类

---
+++ C#_StudyDemo/NestedClass_22/NestedClass_22/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NestedClass_22
+{
+    public class ClassA
+    {
+        private int state = -1;
+        public int State { get {
+                return state;
+            } }
+        public class ClassB
+        {
+            public void setPrivateState(ClassA target, int newState)
+            {
+                target.state = newState;
+            }
+        }
+    }
+
+    class Program
+    {
+        /*
+         * 10.2.3、嵌套的类型定义
+         * 
+         */ 
+        static void Main(string[] args)
+        {
+            ClassA A = new ClassA();
+            WriteLine("A.state = "+A.State);
+            ClassA.ClassB B = new ClassA.ClassB();
+            B.setPrivateState(A,999);
+            WriteLine("A.state = "+A.State);
+            ReadKey();
+        }
+    }
+}
