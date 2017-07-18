6c9b8c9feeaf7f19d2be248f28e95c176ec5e822
SYCHS <429301805@qq.com>
2017-07-18 21:45:52 +0800

    25.利用集合实现扑克牌类库

--- C#_StudyDemo/CardLib_25/CardLib_25/Deck.cs
+++ C#_StudyDemo/CardLib_25/CardLib_25/Deck.cs
@@ -7,16 +7,24 @@ namespace CardLib_25
 {
     public class Deck
     {
-        private Card[] cards;
+        private CardsCollection cards = new CardsCollection();
 
         public Deck()
         {
-            cards = new Card[52];
+            /*cards = new Card[52];
             for (int suitVal = 0; suitVal < 4; suitVal++)
             {
                 for (int rankVal = 1; rankVal < 14; rankVal++)
                 {
                     cards[suitVal * 13 + rankVal - 1] = new Card((Suit)suitVal, (Rank)rankVal);
+                }
+            }*/
+
+            for (int suitVal = 0; suitVal < 4; suitVal++)
+            {
+                for (int rankVal = 1; rankVal < 14; rankVal++)
+                {
+                    cards.Add(new CardLib_25.Card((Suit)suitVal,(Rank)rankVal));
                 }
             }
 
@@ -33,23 +41,26 @@ namespace CardLib_25
 
         public void Shuffle()
         {
-            Card[] newDeck = new Card[52];
+            //Card[] newDeck = new Card[52];
+            CardsCollection newDeck = new CardsCollection();
             bool[] assigned = new bool[52];
             Random sourceGen = new Random();
             for (int i = 0; i < 52; i++)
             {
-                int destCard = 0;
+                //int destCard = 0;
+                int sourceCard = 0;
                 bool foundCard = false;
                 while (foundCard == false)
                 {
-                    destCard = sourceGen.Next(52);//Next(x)----生成一个介于0~x之间的随机数。
-                    if (assigned[destCard] == false)
+                    sourceCard = sourceGen.Next(52);//Next(x)----生成一个介于0~x之间的随机数。
+                    if (assigned[sourceCard] == false)
                         foundCard = true;
                 }
-                assigned[destCard] = true;
-                newDeck[destCard] = cards[i];
+                assigned[sourceCard] = true;
+                //newDeck[destCard] = cards[i];
+                newDeck.Add(cards[sourceCard]);
             }
-            newDeck.CopyTo(cards,0);
+            newDeck.CopyTo(cards);
         }
     }
 }
