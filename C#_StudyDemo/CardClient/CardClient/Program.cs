edd973bd7ac42ed33a6041dd659fe79420af1f43
SYCHS <429301805@qq.com>
2017-07-20 22:00:29 +0800

    给CardLib添加深度复制后的测试代码

--- C#_StudyDemo/CardClient/CardClient/Program.cs
+++ C#_StudyDemo/CardClient/CardClient/Program.cs
@@ -12,7 +12,7 @@ namespace CardClient
     {
         static void Main(string[] args)
         {
-            Deck myDeck = new Deck();
+            /*Deck myDeck = new Deck();
            
             myDeck.Shuffle();
             for (int i = 0; i < 52; i++)
@@ -23,7 +23,17 @@ namespace CardClient
                     Write(", ");
                 else
                     WriteLine();
-            }
+            }*/
+
+            //给CardLib添加深度复制后的测试代码
+            Deck deck1 = new Deck();
+            Deck deck2 = (Deck)deck1.Clone();
+            WriteLine($"The first card in the original deck is : {deck1.GetCard(0)}");
+            WriteLine($"The first card in the cloned deck is : {deck2.GetCard(0)}");
+            deck1.Shuffle();
+            WriteLine("Original deck shuffled.");
+            WriteLine($"The first card in the original deck is : {deck1.GetCard(0)}");
+            WriteLine($"The first card in the cloned deck is : {deck2.GetCard(0)}");
             ReadKey();
         }
     }
