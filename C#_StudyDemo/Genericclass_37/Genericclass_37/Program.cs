9c9086ccfc264aecd16203e86afc4663d1ef6190
SYCHS <429301805@qq.com>
2017-07-30 13:29:57 +0800

    37.定义泛型类

---
+++ C#_StudyDemo/Genericclass_37/Genericclass_37/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Genericclass_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm<Animal> farm = new Farm<Animal>();
+            farm.Animals.Add(new Cow("Rual"));
+            farm.Animals.Add(new Chicken("Donna"));
+            farm.Animals.Add(new Chicken("Mary"));
+            farm.Animals.Add(new SuperCow("Ben"));
+            farm.MakeNoises();
+            Farm<Cow> dairyFarm = farm.GetCows();
+            dairyFarm.FeedTheAnimals();
+            foreach (Cow cow in dairyFarm)
+            {
+                if (cow is SuperCow)
+                {
+                    SuperCow newCow = cow as SuperCow;
+                    try
+                    {
+                        newCow.Fly();
+                    }
+                    catch (NullReferenceException nullre)
+                    {                    
+                        WriteLine("cow is null");
+                    }                                                                               
+                }          
+            }
+            ReadKey();
+        }
+    }
+}
