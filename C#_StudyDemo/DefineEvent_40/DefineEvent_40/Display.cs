0d4f82c88f6797740dcfffc4ed4378f448db6526
SYCHS <429301805@qq.com>
2017-07-31 22:20:31 +0800

    40.定义事件——P305

---
+++ C#_StudyDemo/DefineEvent_40/DefineEvent_40/Display.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DefineEvent_40
+{
+    public class Display
+    {
+        public void DisplayMessage(string message)
+            => WriteLine($"Message arrived : {message}");
+    }
+}
---
+++ C#_StudyDemo/DefineEvent_40/DefineEvent_40/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DefineEvent_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connection myConnection = new Connection();
+            Display myDisplay = new Display();
+            myConnection.MessageArrived += new MessageHandler(myDisplay.DisplayMessage);
+            myConnection.Connect();
+            ReadKey();
+
+        }
+    }
+}
