f6f9d913df8b0d4dee1e424653d19134a498605b
SYCHS <429301805@qq.com>
2017-08-03 19:57:26 +0800

    45.使用匿名类型——P332

---
+++ C#_StudyDemo/AnonymousType_45/AnonymousType_45/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AnonymousType_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var curries = new[]//匿名类型对象的数组
+            {
+                new { MainIngredient = "Lamb",Style = "Dhansak",Spiciness = 5},
+                new { MainIngredient = "Lamb",Style = "Dhansak",Spiciness = 5},
+                new { MainIngredient = "Chicken",Style = "Dhansak",Spiciness = 5},
+            };
+            WriteLine(curries[0].ToString());
+            WriteLine(curries[0].GetHashCode());
+            WriteLine(curries[1].GetHashCode());
+            WriteLine(curries[2].GetHashCode());
+            WriteLine(curries[0].Equals(curries[1]));//比较的是哈希值
+            WriteLine(curries[0].Equals(curries[2]));
+            WriteLine(curries[0] == curries[1]);
+            WriteLine(curries[0] == curries[2]);
+            ReadKey();
+        }
+    }
+}
