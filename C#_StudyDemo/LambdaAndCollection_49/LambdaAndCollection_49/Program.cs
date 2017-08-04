17ca5552aec29b0ef2442185c3b82515de9297b1
SYCHS <429301805@qq.com>
2017-08-04 21:57:17 +0800

    49.使用Lambda表达式和集合——P352，应注意理解

---
+++ C#_StudyDemo/LambdaAndCollection_49/LambdaAndCollection_49/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace LambdaAndCollection_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] curries = { "pathia","jalfrezi","korma"};
+            WriteLine(curries.Aggregate((a,b) => a + " " + b));
+            WriteLine(curries.Aggregate<string,int>(0,(a,b) => a + b.Length));
+            WriteLine(curries.Aggregate<string,string,string>("Some curries:",(a,b) => a +" " + b,a => a));
+            WriteLine(curries.Aggregate<string,string,int>(
+                "Some curries:",
+                (a,b) => a + " " + b,
+                a => a.Length));
+            ReadKey();
+        }
+    }
+}
